3d36347d3dbe566c5cc6509776f56885632fede5
yxc <yxc2020@whu.edu.cn>
2023-11-19 12:24:08 +0800

    Fix some compile or running issues

--- tool/build_trt_engine.sh
+++ tool/build_trt_engine.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-/usr/src/tensorrt/bin/trtexec --onnx=./model/backbone.onnx --fp16 --plugins=build/libpointpillar_core.so --saveEngine=./model/backbone.plan --inputIOFormats=fp16:chw,int32:chw,int32:chw --verbose --dumpLayerInfo --dumpProfile --separateProfileRun --profilingVerbosity=detailed > workspace/backbone.8611.log 2>&1
+
+# ldd /usr/lib/x86_64-linux-gnu/libnvinfer_plugin.so.8
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/x86_64-linux-gnu/
+/usr/src/tensorrt/bin/trtexec --onnx=./model/backbone.onnx --fp16 --plugins=build/libpointpillar_core.so --saveEngine=./model/backbone.plan --inputIOFormats=fp16:chw,int32:chw,int32:chw --verbose --dumpLayerInfo --dumpProfile --separateProfileRun --profilingVerbosity=detailed 
